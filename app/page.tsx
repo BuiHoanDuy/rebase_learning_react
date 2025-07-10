b311e2a961d5a23223ff956f539040cb32edab1f
DuyBH <hoanduy1802@gmail.com>
2025-07-10 11:07:23 +0700

    TQ-001 Add text 3

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,15 @@ export default function HomePage() {
         <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl">HSK Tiêu chuẩn 3</h1>
         <p className="mt-4 text-lg sm:text-xl md:text-2xl">
           Trang thông tin học tập dành cho môn học
-          <br/> TIẾNG TRUNG QUỐC 5
+          Trang chu
+          <br/> TIẾNG TRUNG QUỐC 5 
+
         </p>
         <Link href="/docs" className="mt-8 inline-block bg-gray-900 text-white font-semibold py-2 px-4 rounded hover:bg-gray-700">
           BÀI HỌC
         </Link>
         <Link href="/exercise" className="ml-5 mt-8 inline-block bg-gray-900 text-white font-semibold py-2 px-4 rounded hover:bg-gray-700">
-          BÀI TẬP
+          BÀI TẬP Nho 
         </Link>
       </div>
     </main>

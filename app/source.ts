9aa19d985148e9cc5dc7bd222007fc300e90e1a3
DuyBH <hoanduy1802@gmail.com>
2025-07-10 22:03:06 +0700

    2

--- app/source.ts
+++ app/source.ts
@@ -3,7 +3,7 @@ import { createMDXSource } from 'fumadocs-mdx';
 import { loader } from 'fumadocs-core/source';
 
 export const { getPage, getPages, pageTree } = loader({
-  baseUrl: '/docs',
+  baseUrl: '/docs',baseUrl: '/docs2',
   rootDir: 'docs',
   source: createMDXSource(map),
 });

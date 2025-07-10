87b27a0131ba75ae67b20c17c4ee64d6b04c9d26
DuyBH <hoanduy1802@gmail.com>
2025-07-10 20:54:28 +0700

    ok

--- app/source.ts
+++ app/source.ts
@@ -1,7 +1,7 @@
 import { map } from '@/.map';
 import { createMDXSource } from 'fumadocs-mdx';
 import { loader } from 'fumadocs-core/source';
-
+niin
 export const { getPage, getPages, pageTree } = loader({
   baseUrl: '/docs',
   rootDir: 'docs',

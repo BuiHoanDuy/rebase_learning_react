31cc30a19b11b004a95acb55f6ee8d9b8ef6f20d
DuyBH <125473998+BuiHoanDuy@users.noreply.github.com>
2025-07-11 10:01:06 +0700

    TQ-002 (#8)

--- app/source.ts
+++ app/source.ts
@@ -3,6 +3,8 @@ import { createMDXSource } from 'fumadocs-mdx';
 import { loader } from 'fumadocs-core/source';
 
 export const { getPage, getPages, pageTree } = loader({
+  export const { getPage, getPages, pageTree } = loader({
+    export const { getPage, getPages, pageTree } = loader({
   baseUrl: '/docs',baseUrl: '/docs2',
 
   rootDir: 'docs',rootDir: 'docs2',

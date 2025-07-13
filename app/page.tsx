b9e96c35062520cd447a9bdde6631e20fe20b50e
DuyBH <hoanduy1802@gmail.com>
2025-07-13 15:04:57 +0700

    5

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 //2
 //3
 //4
+//5
 export default function HomePage() {
   return (
     <main className="flex h-screen items-center justify-center bg-black text-white">

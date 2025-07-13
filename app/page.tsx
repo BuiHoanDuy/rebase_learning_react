86faf3aefae3fd1e0aef4f3b01e3d51c3845a3a6
DuyBH <125473998+BuiHoanDuy@users.noreply.github.com>
2025-07-13 16:00:47 +0700

    reset4 (#17)

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 //3
 //4
 //5
+//7
 export default function HomePage() {
   return (
     <main className="flex h-screen items-center justify-center bg-black text-white">

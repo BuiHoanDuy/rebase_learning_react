c1a228703315ea3c1d2be45d51dd1fa9e78a69bc
DuyBH <hoanduy1802@gmail.com>
2025-07-13 14:57:20 +0700

    3_2

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 //import HLLogo1 from '~/assets/images/brand/logo01.png';
 //1
 //2
+//3
 export default function HomePage() {
   return (
     <main className="flex h-screen items-center justify-center bg-black text-white">
